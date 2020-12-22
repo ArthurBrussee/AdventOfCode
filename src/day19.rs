f48b7565ac267d9cc4a3239dbee451e69457fbf9
Arthur Brussee <arthur.brussee@gmail.com>
2020-12-22 01:25:03 +0100

    Day 19, refactor main

---
+++ src/day19.rs
@@ -0,0 +1,81 @@
+use std::collections::HashMap;
+use std::fs;
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
+enum Token {
+    Number(u32),
+    Times,
+    Plus,
+    OpenParen,
+    CloseParen,
+}
+
+pub fn calc() -> (u64, u64) {
+    let file_str = fs::read_to_string("./inputs/day18.txt").expect("Can't find input file.");
+    let tokens = file_str.lines().map(|l| tokenize(l)).collect::<Vec<_>>();
+    let p1 = tokens.iter().map(|l| evaluate(l, 1, 1)).sum();
+    let p2 = tokens.iter().map(|l| evaluate(l, 2, 1)).sum();
+    (p1, p2)
+}
+
+fn tokenize(s: &str) -> Vec<Token> {
+    s.replace(" ", "")
+        .chars()
+        .map(|s| match s {
+            '*' => Token::Times,
+            '+' => Token::Plus,
+            '(' => Token::OpenParen,
+            ')' => Token::CloseParen,
+            _ => Token::Number(s.to_string().parse().unwrap()),
+        })
+        .collect()
+}
+
+fn evaluate(tokens: &Vec<Token>, prec_add: i32, prec_times: i32) -> u64 {
+    let mut prec = HashMap::<Token, i32>::new();
+    prec.insert(Token::Times, prec_times);
+    prec.insert(Token::Plus, prec_add);
+    prec.insert(Token::OpenParen, i32::MIN);
+
+    let mut op_stack = Vec::new();
+    let mut postfix = Vec::new();
+
+    for token in tokens {
+        match token {
+            Token::Number(_) => postfix.push(*token),
+            Token::OpenParen => op_stack.push(*token),
+            Token::CloseParen => loop {
+                let top_token = op_stack.pop().unwrap();
+                if top_token == Token::OpenParen {
+                    break;
+                }
+                postfix.push(top_token);
+            },
+            Token::Plus | Token::Times => {
+                while !op_stack.is_empty() && prec[op_stack.last().unwrap()] >= prec[token] {
+                    postfix.push(op_stack.pop().unwrap());
+                }
+                op_stack.push(*token)
+            }
+        }
+    }
+
+    postfix.extend(op_stack.iter().rev());
+
+    let mut vm = Vec::<u64>::new();
+    for token in postfix {
+        match token {
+            Token::Number(num) => vm.push(num as u64),
+            Token::Plus => {
+                let (left, right) = (vm.pop().unwrap(), vm.pop().unwrap());
+                vm.push(left + right);
+            }
+            Token::Times => {
+                let (left, right) = (vm.pop().unwrap(), vm.pop().unwrap());
+                vm.push(left * right);
+            }
+            _ => unreachable!(),
+        }
+    }
+    vm[0]
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::fmt::Display;
 use std::time::Instant;
 
@@ -11,6 +12,7 @@ mod day15;
 mod day16;
 mod day17;
 mod day18;
+mod day19;
 mod day2;
 mod day3;
 mod day4;
@@ -20,69 +22,58 @@ mod day7;
 mod day8;
 mod day9;
 
-fn print_solution(day: impl Display, solution: (impl Display, impl Display), start: Instant) {
-    let ms = start.elapsed().as_secs_f64() * 1000.0;
-    println!("Day {}, {:.2}ms: ({}, {})", day, ms, solution.0, solution.1);
+fn run_solution<T, V>(day: u32, calc: fn() -> (T, V))
+where
+    T: Display,
+    V: Display,
+{
+    let now = Instant::now();
+    let (p1, p2) = calc();
+    let ms = now.elapsed().as_secs_f64() * 1000.0;
+    println!("Day {}, {:.2}ms: ({}, {})", day, ms, p1, p2);
 }
 
 fn main() {
     println!("AOC 2020!");
 
+    let mut days = env::args()
+        .filter_map(|arg| arg.strip_prefix("--day").map(|f| f.to_string()))
+        .map(|r| r.parse::<usize>().unwrap() - 1)
+        .collect::<Vec<_>>();
+
+    if days.len() == 0 {
+        days.extend(0..=25);
+    }
+
     let start_run = Instant::now();
 
-    let now = Instant::now();
-    print_solution(1, day1::calc(), now);
+    let funcs = [
+        || run_solution(1, day1::calc),
+        || run_solution(2, day2::calc),
+        || run_solution(3, day3::calc),
+        || run_solution(4, day4::calc),
+        || run_solution(5, day5::calc),
+        || run_solution(6, day6::calc),
+        || run_solution(7, day7::calc),
+        || run_solution(8, day8::calc),
+        || run_solution(9, day9::calc),
+        || run_solution(10, day10::calc),
+        || run_solution(11, day11::calc),
+        || run_solution(12, day12::calc),
+        || run_solution(13, day13::calc),
+        || run_solution(14, day14::calc),
+        || run_solution(15, day15::calc),
+        || run_solution(16, day16::calc),
+        || run_solution(17, day17::calc),
+        || run_solution(18, day18::calc),
+        || run_solution(19, day19::calc),
+    ];
 
-    let now = Instant::now();
-    print_solution(2, day2::calc(), now);
-
-    let now = Instant::now();
-    print_solution(3, day3::calc(), now);
-
-    let now = Instant::now();
-    print_solution(4, day4::calc(), now);
-
-    let now = Instant::now();
-    print_solution(5, day5::calc(), now);
-
-    let now = Instant::now();
-    print_solution(6, day6::calc(), now);
-
-    let now = Instant::now();
-    print_solution(7, day7::calc(), now);
-
-    let now = Instant::now();
-    print_solution(8, day8::calc(), now);
-
-    let now = Instant::now();
-    print_solution(9, day9::calc(), now);
-
-    let now = Instant::now();
-    print_solution(10, day10::calc(), now);
-
-    let now = Instant::now();
-    print_solution(11, day11::calc(), now);
-
-    let now = Instant::now();
-    print_solution(12, day12::calc(), now);
-
-    let now = Instant::now();
-    print_solution(13, day13::calc(), now);
-
-    let now = Instant::now();
-    print_solution(14, day14::calc(), now);
-
-    let now = Instant::now();
-    print_solution(15, day15::calc(), now);
-
-    let now = Instant::now();
-    print_solution(16, day16::calc(), now);
-
-    let now = Instant::now();
-    print_solution(17, day17::calc(), now);
-
-    let now = Instant::now();
-    print_solution(18, day18::calc(), now);
+    for d in days {
+        if let Some(f) = funcs.get(d) {
+            f();
+        }
+    }
 
     println!(
         "Done with AOC! Took {:.2}ms",
