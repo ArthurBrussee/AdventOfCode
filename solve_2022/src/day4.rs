b523d936173fd41ffa83cedc0fbf3d78a1e37a05
Arthur Brussee <arthur.brussee@gmail.com>
2022-12-04 13:49:08 +0000

    Day 4

---
+++ solve_2022/src/day4.rs
@@ -0,0 +1,33 @@
+use std::ops::RangeInclusive;
+
+pub fn calc(input: &str) -> (usize, usize) {
+    input
+        .lines()
+        .map(|line| {
+            fn to_range(l: &str) -> RangeInclusive<usize> {
+                let (min, max) = l.split_once('-').unwrap();
+                min.parse().unwrap()..=max.parse().unwrap()
+            }
+
+            let ranges: [_; 2] = line
+                .split(',')
+                .map(to_range)
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap();
+            let (r1, r2) = (&ranges[0], &ranges[1]);
+
+            let overlap_full = r1.contains(r2.start()) && r1.contains(r2.end())
+                || r2.contains(r1.start()) && r2.contains(r1.end());
+            let overlap_part = overlap_full || r1.contains(r2.start()) || r1.contains(r2.end());
+            (usize::from(overlap_full), usize::from(overlap_part))
+        })
+        .fold((0, 0), |(acc1, acc2), (p1, p2)| (acc1 + p1, acc2 + p2))
+}
+
+#[test]
+fn test() {
+    let (p1, p2) = calc(&aoc_lib::read_file(2021, 4, true));
+    assert_eq!(p1, 2);
+    assert_eq!(p2, 4);
+}
--- solve_2022/src/lib.rs
+++ solve_2022/src/lib.rs
@@ -3,12 +3,14 @@ use aoc_lib::run_solution;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 pub fn run() {
     let funcs = [
         || run_solution(2022, 1, day1::calc),
         || run_solution(2022, 2, day2::calc),
         || run_solution(2022, 3, day3::calc),
+        || run_solution(2022, 4, day4::calc),
     ];
 
     aoc_lib::run_solutions(&funcs);
